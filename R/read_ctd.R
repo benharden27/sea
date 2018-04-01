d4ad0e3e0b2c71344fa53a57d616068c324f4d62
benharden27 <benharden27@gmail.com>
2018-04-01 15:22:28 -0400

    create legacy file for depreciated ctd read functions

---
+++ R/read_ctd.R
@@ -0,0 +1,174 @@
+# Functions to read in and process data from CNV files
+
+
+#' Read and process CTD data
+#'
+#' Wrapper function for oce functions read.ctd, ctdTrim, and ctdDecimate.
+#' Extracts and adds metadata from header that is unique to SEA headers.
+#'
+#' @param cnv_file file path to the CNV file to be read
+#' @param pmin minimum cut off pressure for ctdTrim
+#' @param p pressure bins for ctdDecimate
+#' @param ... additional arguments passed to called functions
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_ctd <- function(cnv_file, pmin = 5, p = 1, ...) {
+
+
+# Initial read ------------------------------------------------------------
+
+  # Read CTD data from file
+  ctd <- oce::read.ctd(cnv_file, ...)
+
+  # Trim CTD data to remove upcast and surface values
+  ctd <- oce::ctdTrim(ctd, parameters = list(pmin = pmin), ...)
+
+  # Bin the CTD data into consistent pressure bins
+  ctd <- oce::ctdDecimate(ctd, p = 1, ...)
+
+# Extract metadata --------------------------------------------------------
+
+  X <- readLatLon(cnv_file)
+
+
+
+
+
+}
+
+
+
+#' Read lat and lon from CNV header
+#'
+#' @param cnv_file cnv file to read
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_cnv_latlon <- function(cnv_file) {
+
+  r <- readr::read_lines(filein,n_max = 100)
+
+  # set possible patterns to search for
+  patt <- "([0-9]+[^0-9]+[0-9]+[^0-9]+[0-9]*)"
+  patt2 <- "([0-9]+[^0-9]+[0-9])"
+  patt3 <- "([0-9]+)"
+
+  # LATITUDE
+  # switch depending on what format the lon and lat are stored as
+  if(length(grep('^.*Lat.*Lon.*$',r)) > 0) {
+    case <- 1
+    line <- grep('^.*Lat.*Lon.*$',r)
+  } else if (length(grep('Lat|Lat',r,ignore.case=T))==0) {
+    case <- 2
+    line <- 1
+  } else {
+    case <- 3
+    line <- grep("Lat",r,ignore.case=T)[1] # finds the word "Latitude" in r
+  }
+
+  # search for the patterns in order
+  a <- regexpr(patt,r[line])
+  if (a==-1) {
+    a <- regexpr(patt2,r[line])
+    if(a==-1) {
+      a <- regexpr(patt3,r[line])
+    }
+  }
+
+  # assign the latitude based on the search findings
+  lat <- substr(r[line],a,a+attr(a,"match.length")-1)
+  lat <- strsplit(lat,"[^0-9\\.]")[[1]]
+  lat <- lat[lat!=""] # removes blank sections
+
+  # Define Hemisphere
+  # hemi <- substr(r[line],regexpr("[NS]",r[line]),regexpr("[NS]",r[line]))
+  hemi <- substr(r[line],regexpr("[NS].{0,5}$",r[line]),regexpr("[NS].{0,5}$",r[line]))
+  if(hemi=='S'){
+    fac <- -1
+  } else {
+    fac <- 1
+  }
+
+  # depending on the end format of "lat" do various different things to parse the output
+  if(length(lat)==1) {
+    if(length(strsplit(lat,"\\.")[[1]])<3) {
+      lat <- fac*as.numeric(substr(lat,1,2)) + fac * as.numeric(substr(lat,3,100))/60
+    } else {
+      lat <- strsplit(lat,"\\.")[[1]]
+      lat <- fac * as.numeric(lat[1]) + fac * (as.numeric(lat[2])+as.numeric(lat[3])/10)/60
+    }
+  } else {
+    lat <- fac * as.numeric(lat[1]) + fac * as.numeric(lat[2])/60;
+  }
+
+
+  # LONGITUDE
+
+  # again, switch by the format of the line
+  if (case==1) {
+    rest<- substr(r[line],a+attr(a,"match.length"),100)
+  } else if (case==2) {
+    rest <- 'xxxxx'
+  } else {
+    rest <- r[grep("Lon",r,ignore.case = T)[1]]
+  }
+
+  # search for the patterns
+  a <- regexpr(patt,rest)
+  if (a==-1) {
+    a <- regexpr(patt2,rest)
+    if(a==-1) {
+      a <- regexpr(patt3,rest)
+    }
+  }
+
+  # assign longitude based on patterns
+  lon <- substr(rest,a,a+attr(a,"match.length")-1)
+  lon <- strsplit(lon,"[^0-9\\.]")[[1]]
+  lon <- lon[lon!=""] # removes blank sections
+
+  # Define Hemisphere
+  # hemi <- substr(rest,regexpr("[WE]",rest),regexpr("[WE]",rest));
+  hemi <- substr(rest,regexpr("[WE].{0,5}$",rest),regexpr("[WE].{0,5}$",rest));
+  if(hemi=='W'){
+    fac <- -1
+  } else {
+    fac <- 1
+  }
+
+
+  # do various formating based on type of "lon" output
+  if(length(lon)==1) {
+    if(length(strsplit(lon,"\\.")[[1]])==2) {
+      lon <- fac * as.numeric(substr(lon,1,2)) + fac * as.numeric(substr(lon,3,100))/60
+    } else if (nchar(lon)>3) {
+      lon <- strsplit(lon,"\\.")[[1]]
+      lon <- fac * as.numeric(lon[1]) +fac * (as.numeric(lon[2])+as.numeric(lon[3])/10)/60
+    } else {
+      lon <- fac*as.numeric(lon)
+    }
+  } else {
+    lon <- fac* as.numeric(lon[1]) + fac * as.numeric(lon[2])/60;
+  }
+
+  # show the lines of output for when there is no lon or no lat
+  if(is.na(lon)|is.na(lat)) {
+    show(r)
+    # a<-readline('Press enter key to continue...')
+  }
+
+  X <- NULL
+  X$lon <- lon
+  X$lat <- lat
+  X$r <- r
+
+  return(X)
+
+
+
+}
