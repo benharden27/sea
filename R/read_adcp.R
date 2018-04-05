be4849e152e98aab71fe6b04869469a3455332c1
benharden27 <benharden27@gmail.com>
2018-04-05 15:28:03 -0400

    add ability to stack ADCP text files

--- R/read_adcp.R
+++ R/read_adcp.R
@@ -14,12 +14,12 @@ read_adcp <- function(adcp_file) {
   r <- readr::read_lines(adcp_file)
 
   # finds the header line index and return headerline
-  hi <- str_which(r, "^Ens.*Eas")
+  hi <- stringr::str_which(r, "^Ens.*Eas")
 
-  headers <- read_tsv(adcp_file, skip = hi-1, n_max = 0) %>%
+  headers <- readr::read_tsv(adcp_file, skip = hi-1, n_max = 0) %>%
     names() %>%
-    str_extract_all("\\w") %>%
-    map(str_flatten) %>%
+    stringr::str_extract_all("\\w") %>%
+    purrr::map(stringr::str_flatten) %>%
     unlist()
 
   # read the table in from 3 lines past the header onwards
@@ -27,10 +27,10 @@ read_adcp <- function(adcp_file) {
   names(a) <- headers
 
   # find the indexes of u, v, lon and lat from titles in headerline
-  ui <- str_which(names(a),"Eas")
-  vi <- str_which(names(a),"Nor")
-  loni <- str_which(names(a),"FLon")
-  lati <- str_which(names(a),"FLat")
+  ui <- stringr::str_which(names(a),"Eas")
+  vi <- stringr::str_which(names(a),"Nor")
+  loni <- stringr::str_which(names(a),"FLon")
+  lati <- stringr::str_which(names(a),"FLat")
 
   # extract u and v, lon and lat
   u <- as.matrix(a[ui])
@@ -60,15 +60,30 @@ read_adcp <- function(adcp_file) {
 #' @export
 #'
 #' @examples
-read_adcp_fold <- function(adcp_fold, pattern = "1-30") {
+read_adcp_fold <- function(adcp_fold, stack = T) {
 
-  files <- list.files(adcp_fold, pattern = paste0("ADCP.*",pattern,"\\.txt"))
+  pattern = "1-30"
+  files30 <- list.files(adcp_fold, pattern = paste0("ADCP.*1-30\\.txt"))
+  files60 <- list.files(adcp_fold, pattern = paste0("ADCP.*31-60\\.txt"))
+
+  file_start30 <- stringr::str_extract(files30,".*_.*_.*_")
+  file_start60 <- stringr::str_extract(files60,".*_.*_.*_")
 
   Y <- list(lon = NULL, lat = NULL, dttm = lubridate::ymd_hms(NULL), u = NULL, v= NULL)
-  for (i in 1:length(files)) {
-    adcp_file <- file.path(adcp_fold, files[i])
-    cat(adcp_file)
-    X <- read_adcp(adcp_file)
+
+  for (i in 1:length(files30)) {
+    adcp_file30 <- file.path(adcp_fold, files30[i])
+    X <- read_adcp(adcp_file30)
+    if (stack & file_start30[1] %in% file_start60) {
+      adcp_file60 <- file.path(adcp_fold, paste0(file_start30[i],"31-60.txt"))
+      X2 <- read_adcp(adcp_file60)
+      X$u <- cbind(X$u,X2$u)
+      X$v <- cbind(X$v,X2$v)
+    } else {
+      X$u <- cbind(X$u,matrix(NA,dim(X$u)))
+      X$v <- cbind(X$v,matrix(NA,dim(X$v)))
+    }
+
     Y$lon <- append(Y$lon,X$lon)
     Y$lat <- append(Y$lat,X$lat)
     Y$dttm <- append(Y$dttm,X$dttm)
@@ -79,3 +94,4 @@ read_adcp_fold <- function(adcp_fold, pattern = "1-30") {
   return(Y)
 
 }
+
