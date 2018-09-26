ecc47a422a0190c8152097e24ffec42d0b74a665
benharden27 <benharden27@gmail.com>
2018-09-26 12:01:40 -0400

    add functions to format packaged data for ODV

---
+++ R/odv.R
@@ -0,0 +1,137 @@
+#' Format Houlry Data for ODV inport
+#'
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+format_hourly_odv <- function(data,file,cruiseID = NULL) {
+
+# Try and determine the name of the cruise from the data input if not specified. If not, assign "unknown".
+  if(is.null(cruiseID)) {
+    cruiseID <- deparse(substitute(data))
+    if(!stringr::str_detect(stringr::str_sub(cruiseID,1,1),"[C|S|c|s]")) {
+      cruiseID = "unknown"
+    }
+  }
+
+  # Convert from sea structure to get the hourly part
+  if(is_sea_struct(data)){
+    data <- data$hourly
+  }
+
+  odv_out <- tibble::tibble(
+    Cruise = cruiseID,
+    Station = 1:dim(data)[1],
+    Type = "C",
+    `mon/day/yr` = format(data$dttm,"%m/%d/%Y"),
+    `Lon (∞E)` = data$lon,
+    `Lat (∞N)` = data$lat,
+    `Bot. Depth [m]` = data$depth_bot,
+    `Depth [m]` = 0,
+    `Temperature [∞C]` = data$temp,
+    `Salinity [psu]` = data$sal,
+    `Fluorescence` = data$fluor,
+    `hh:mm` = format(data$dttm,"%H:%M"),
+    `Wind Speed [knots]` = data$wind_sp,
+    `Wind Direction [deg]` = data$wind_dir,
+    `Wind-E/W Comp. [m/s]` = 0,
+    `Wind-N/S Comp. [m/s]` = 0,
+    `CDOM 1 min Avg` = data$cdom_1min,
+    `Xmiss 1 min Avg` = data$xmiss_1min
+  )
+
+  readr::write_tsv(odv_out,file)
+
+  return(odv_out)
+
+}
+
+#' Format Neuston Data for ODV inport
+#'
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+format_neuston_odv <- function(data,file,cruiseID = NULL) {
+
+  # Try and determine the name of the cruise from the data input if not specified. If not, assign "unknown".
+  if(is.null(cruiseID)) {
+    cruiseID <- deparse(substitute(data))
+    if(!stringr::str_detect(stringr::str_sub(cruiseID,1,1),"[C|S|c|s]")) {
+      cruiseID = "unknown"
+    }
+  }
+
+  # Convert from sea structure to get the hourly part
+  if(is_sea_struct(data)){
+    data <- data$neuston
+  }
+
+  odv_out <- tibble::tibble(
+    Cruise = cruiseID,
+    Station = 1:dim(data)[1],
+    Type = "X",
+    `mon/day/yr` = format(data$dttm_in,"%m/%d/%Y"),
+    `hh:mm` = format(data$dttm_in,"%H:%M"),
+    `Lon (∞E)` = data$lon,
+    `Lat (∞N)` = data$lat,
+    `Depth [m]` = 0
+  )
+
+  ii <- which(names(data) == "temp")
+
+  odv_out <- dplyr::bind_cols(odv_out,data[9:dim(data)[2]])
+
+  readr::write_tsv(odv_out,file)
+
+  return(odv_out)
+
+}
+
+#' Format Surface Data for ODV inport
+#'
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+format_surface_odv <- function(data,file,cruiseID = NULL) {
+
+  # Try and determine the name of the cruise from the data input if not specified. If not, assign "unknown".
+  if(is.null(cruiseID)) {
+    cruiseID <- deparse(substitute(data))
+    if(!stringr::str_detect(stringr::str_sub(cruiseID,1,1),"[C|S|c|s]")) {
+      cruiseID = "unknown"
+    }
+  }
+
+  # Convert from sea structure to get the hourly part
+  if(is_sea_struct(data)){
+    data <- data$surfsamp
+  }
+
+  odv_out <- tibble::tibble(
+    Cruise = cruiseID,
+    Station = 1:dim(data)[1],
+    Type = "X",
+    `mon/day/yr` = format(data$dttm_local,"%m/%d/%Y"),
+    `hh:mm` = format(data$dttm_local,"%H:%M"),
+    `Lon (∞E)` = data$lon,
+    `Lat (∞N)` = data$lat,
+    `Depth [m]` = 0
+  )
+
+  ii <- which(names(data) == "temp")
+
+  odv_out <- dplyr::bind_cols(odv_out,data[9:dim(data)[2]])
+
+  readr::write_tsv(odv_out,file)
+
+  return(odv_out)
+
+}
